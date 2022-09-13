9ea1a7fb7a47f64d735b346c5882a8d3d92432eb
Aaron Joss <a.s.joss@hotmail.co.uk>
2022-09-13 15:04:47 +0100

    adding JavaScript file

---
+++ script.js
@@ -0,0 +1,11 @@
+let quotes = [
+    "Love is blind.", "A bird in the hand is worth two in the bush.", 
+    "You can\'t make an omelet without breaking some eggs.", 
+    "Absence makes the heart grow fonder.", "No pain, no gain.", 
+    "Every cloud has a silver lining.", "If you can\'t beat \'em, join \'em.",
+    "Just be yourself", "Stop worrying", "Don\'t worry", "Fake it until you make it",
+    "Man up", "Take the emotion out of it", "Don\'t worry about what other people think about you.",
+    "If they don\'t like you, they\'re just jealous of you.", "Don\'t take things so personally.",
+    "Sticks and stones may break your bones but words can never hurt you.", 
+    "Good things come to those who wait.", "Follow your passion",
+]
